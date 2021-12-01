a3d4c7842f9472a512a80547491bf768e39cbd96
KrutonS <krutonpl@gmail.com>
2021-12-01 13:52:59 +0100

    moved sass variables to separate file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 // import { useRouter } from 'next/dist/client/router';
 import Header from '../components/header';
