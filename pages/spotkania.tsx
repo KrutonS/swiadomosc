8de2fff59761503894fee566c9e856fedfcc30e1
KrutonS <krutonpl@gmail.com>
2021-12-16 12:33:09 +0100

    AllMeetings CMS request

--- pages/spotkania.tsx
+++ pages/spotkania.tsx
@@ -1,17 +1,43 @@
-import { FC } from 'react';
+import datoReq from 'lib/datocms';
+import { GetStaticProps, NextPage } from 'next';
+import { Meeting } from 'types';
 import Calendar from '../components/calendar';
 import styles from '../styles/Meetings.module.scss';
 
-const MeetingsPage: FC = () => {
+type QueryResponse = { meetings: Meeting[] };
+
+const MeetingsPage: NextPage<QueryResponse> = ({ meetings }) => {
+	// console.log(allMeetings);
+
 	return (
 		<main className={styles.main}>
 			<div className={styles.heading}>
 				<h1 className={styles.title}>Spotkania</h1>
 				<p className={styles['title-p']}>Chciałbym zapisać się na...</p>
 			</div>
-			<Calendar />
+			<Calendar meetings={meetings} />
 		</main>
 	);
 };
+export const getStaticProps: GetStaticProps<QueryResponse> = async () => {
+	// const allMeetings = (await getAllMeetings()) ?? [];
+	const query = `
+	query AllMeetings{
+		allMeetings {
+			id
+			name
+			weekly
+			startTime
+			length
+		}
+	}`;
+	const { allMeetings: meetings } = await datoReq<
+		{ allMeetings: Meeting[] },
+		never
+	>(query);
+	// const grouped = meetingsByWeekly(allMeetings);
+	// return { props: { meetings: grouped } };
 
+	return { props: { meetings } };
+};
 export default MeetingsPage;
