076d476ae11cc62c7380ea59b20f38d1e2ed97f1
KrutonS <krutonpl@gmail.com>
2021-12-28 15:59:30 +0100

    button mechanic rework

--- components/button.tsx
+++ components/button.tsx
@@ -1,41 +1,63 @@
-import { FC, MouseEventHandler, ReactNode } from 'react';
+import {
+	AnchorHTMLAttributes,
+	ButtonHTMLAttributes,
+	forwardRef,
+	MouseEventHandler,
+	ReactNode,
+	RefObject,
+} from 'react';
 import Link from 'next/link';
+import cn from 'classnames';
 
 import styles from '../styles/Button.module.scss';
 import { ButtonTypes } from '../types';
 
-interface ClickProps {
+type ClickProps = {
 	onClick?: MouseEventHandler;
 	type?: ButtonTypes;
-}
-interface LinkProps {
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+type LinkProps = {
 	href: string;
-}
+} & AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export type ButtonProps = {
 	className?: string;
 	children: ReactNode;
 } & (ClickProps | LinkProps);
 
-const Button: FC<ButtonProps> = props => {
-	const { className = '', children } = props;
-	const nodeClass = `${className} ${styles.btn}`;
-	if ('href' in props) {
-		const { href } = props;
+const Button = forwardRef<HTMLElement | HTMLAnchorElement, ButtonProps>(
+	({ children, className, ...props }, ref) => {
+		// const { className = '', children } = props;
+		const nodeClass = cn(className, styles.btn);
+		if ('href' in props) {
+			const { href, ...attrs } = props;
+			return (
+				<Link href={href}>
+					<a
+						className={nodeClass}
+						ref={ref as RefObject<HTMLAnchorElement>}
+						{...attrs}
+					>
+						{children}
+					</a>
+				</Link>
+			);
+		}
+		const { type = 'button', onClick, ...attrs } = props;
 		return (
-			<Link href={href}>
-				{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-				<a className={nodeClass}>{children}</a>
-			</Link>
+			<button
+				// eslint-disable-next-line react/button-has-type
+				type={type}
+				onClick={onClick}
+				className={nodeClass}
+				ref={ref as RefObject<HTMLButtonElement>}
+				{...attrs}
+			>
+				{children}
+			</button>
 		);
 	}
-	const { type = 'button', onClick } = props;
-	return (
-		// eslint-disable-next-line react/button-has-type
-		<button type={type} onClick={onClick} className={nodeClass}>
-			{children}
-		</button>
-	);
-};
+);
 
 export default Button;
