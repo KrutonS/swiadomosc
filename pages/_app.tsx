a492ab2bee8851f1d0b9cd869616dee6d74bf873
KrutonS <krutonpl@gmail.com>
2022-01-25 18:24:13 +0100

    add toast container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import { UserProvider } from 'utils/contexts/user';
 import { AppProps } from 'next/app';
 import { AuthDialogProvider } from 'utils/contexts/auth-dialog';
 import '../styles/globals.scss';
+import { ToastContainer } from 'react-toastify';
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<UserProvider>
 			<AuthDialogProvider>
+				<ToastContainer />
 				<Component {...pageProps} />
 			</AuthDialogProvider>
 		</UserProvider>
