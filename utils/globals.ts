9ba8fe285e7ba59419fda90f02c462953e082854
KrutonS <krutonpl@gmail.com>
2022-02-03 21:59:43 +0100

    add nbsp

--- utils/globals.ts
+++ utils/globals.ts
@@ -3,3 +3,4 @@ export const emailRegex =
 export const passRegex =
 	// eslint-disable-next-line no-useless-escape
 	/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`! @#$%^&*()_\-+={[}\]|\\:;"'<,>.?\/])/;
+export const nbspU = '\u00a0';
