713fbf1dfea70b46860d0b8f8ac9baabb3ee38c5
KrutonS <krutonpl@gmail.com>
2022-02-03 22:01:17 +0100

    fix required on second input

--- components/user-inputs/input-compare.tsx
+++ components/user-inputs/input-compare.tsx
@@ -1,7 +1,9 @@
-import { FieldValues, UseFormSetError } from 'react-hook-form';
+import { FieldValues } from 'react-hook-form';
+import { FieldsNotTheSameError } from 'utils/errors';
 import Input, { InputProps } from './input';
 
-export type MergeWithCompare<T> = T &
+export type CompareInputsFrom<T> = T &
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	Partial<Record<`compare ${Extract<keyof T, string>}`, any>>;
 
 type Props<T extends FieldValues> = InputProps<T>;
@@ -10,8 +12,8 @@ export const InputCompare = <T extends FieldValues>({
 	id,
 	label,
 	errors,
-	required,
 	options,
+	required = true,
 	...passProps
 }: Props<T>) => {
 	const comparerKey = `compare ${id}`;
@@ -28,31 +30,24 @@ export const InputCompare = <T extends FieldValues>({
 			/>
 			<Input
 				{...passProps}
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 				id={comparerKey as any}
 				label={`Powtórz ${label.toLowerCase()}`}
 				errors={errors && errors[comparerKey]}
+				required={required}
 				autoComplete="off"
 			/>
 		</>
 	);
 };
 
-export function checkCompares<T extends FieldValues>(
-	data: T,
-	setError: UseFormSetError<T>
-): boolean {
-	let isValid = true;
+export function checkCompares<T extends FieldValues>(data: T): void {
 	Object.keys(data).forEach(key => {
 		const compare = data[key];
 		const compareToKey = `compare ${key}`;
 		const compareTo = data[compareToKey];
-		if (compare !== compareTo && compareTo !== undefined) {
-			isValid = false;
-			setError(compareToKey as any, {
-				message: 'Pola nie są takie same!',
-				type: 'compare',
-			});
-		}
+
+		if (compareTo !== undefined && compare !== compareTo)
+			throw new FieldsNotTheSameError(compareToKey);
 	});
-	return isValid;
 }
