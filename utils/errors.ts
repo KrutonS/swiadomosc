4e75d7718257e53de5ece3fec8a341e1ea08ea69
KrutonS <krutonpl@gmail.com>
2022-02-03 21:53:19 +0100

    basic error system

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,27 +1,70 @@
+/* eslint-disable max-classes-per-file */
 import { FirebaseError } from 'firebase/app';
-import { toast } from 'react-toastify';
+import { FieldValues } from 'react-hook-form';
+import { EmailWIthPassword } from 'types';
 
-export class UserNotVerifiedError extends Error {
-	constructor() {
-		super(
-			'Twoje konto nie zostało jeszcze przez Ciebie zweryfikowane. Sprawdź swoja skrzynkę pocztową'
-		);
+export type FirebaseErrors =
+	| 'auth/email-already-in-use'
+	| 'auth/network-request-failed'
+	| 'auth/invalid-email'
+	| 'auth/user-disabled'
+	| 'auth/user-not-found'
+	| 'auth/wrong-password';
+
+export type CustomFormErrors = 'user-not-verified' | 'fields-not-equal';
+
+export type FormErrors = FirebaseErrors | CustomFormErrors;
+
+export const authErrors: { [k in FormErrors]: string } = {
+	'auth/email-already-in-use': 'Email jest już w użyciu. Zapomniałeś hasła?',
+	'auth/network-request-failed': 'Brak połączenia, spróbuj ponownie później',
+	'auth/invalid-email': 'Nieprawidłowy email.',
+	'auth/user-disabled': 'Twoje konto jest zablokowane.',
+	'auth/user-not-found': 'Błędny email bądź hasło',
+	'auth/wrong-password': 'Błędny email bądź hasło',
+	'user-not-verified':
+		'Twoje konto nie zostało jeszcze przez Ciebie aktywowane. Sprawdź swoją skrzynkę pocztową',
+	'fields-not-equal': 'Pola nie są takie same!',
+};
+const defaultError =
+	'Coś poszło nie tak. Skontaktuj się z nami i zgłoś błąd lub spróbuj ponownie później.';
+
+export class FormError<T extends FieldValues> extends FirebaseError {
+	field?: Extract<keyof T, string>;
+
+	constructor(code: FormErrors, field?: Extract<keyof T, string>) {
+		super(code, authErrors[code] || defaultError);
+		this.field = field;
 	}
 }
-export function getErrorMessage(e: unknown): string | unknown {
-	if (typeof e === 'string') return e;
-	if (e instanceof FirebaseError) return e.code;
-	if (e instanceof Error) return e.message;
-	// throw e;
-	return e;
+
+export class UserNotVerifiedError extends FormError<EmailWIthPassword> {
+	constructor() {
+		super('user-not-verified');
+	}
 }
 
-export const handleDefaultError = (e: unknown, handler = toast.error) => {
-	let message =
-		'Coś poszło nie tak. Skontaktuj się z nami i zgłoś błąd lub spróbuj ponownie później.';
+export class FieldsNotTheSameError<T extends FieldValues> extends FormError<T> {
+	constructor(field: Extract<keyof T, string>) {
+		super('fields-not-equal', field);
+	}
+}
+
+export function getErrorMessage(e: unknown, strict = true): string {
+	if (e instanceof FirebaseError)
+		return authErrors[e.code as FirebaseErrors] || e.message;
+	if (!strict) {
+		if (e instanceof Error) return e.message;
+		if (typeof e === 'string') return e;
+		return 'Unknown error';
+	}
+	throw e;
+}
+
+export const getDefaultError = (e: unknown): string => {
+	let message = defaultError;
 	if (typeof e === 'string') message += e;
 	else if (e instanceof FirebaseError) message += e.code;
 	else if (e instanceof Error) message += e.message;
-	handler(message);
 	return message;
 };
---
+++ utils/hooks/errors.ts
@@ -0,0 +1,44 @@
+import { FirebaseError } from 'firebase/app';
+import { useEffect, useState } from 'react';
+import {
+	FieldValues,
+	UseFormSetError,
+	Path,
+	useWatch,
+	Control,
+} from 'react-hook-form';
+import { FirebaseErrors, FormError, getErrorMessage } from 'utils/errors';
+
+export const useFormError = <F extends FieldValues>(
+	setError: UseFormSetError<F>,
+	errorsToInputs: Partial<Record<FirebaseErrors, Path<F>>>,
+	control: Control<F>,
+	shouldFocus?: boolean
+) => {
+	const [generalError, setGeneralError] = useState<string>();
+	const values = useWatch({ control });
+	const refreshError = () => {
+		if (generalError) setGeneralError(undefined);
+	};
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(refreshError, [values]);
+	const onError = (e: unknown) => {
+		const message = getErrorMessage(e);
+		let field: null | Path<F> = null;
+		if (e instanceof FormError && e.field) {
+			field = e.field as Path<F>;
+			// setError(field, { message });
+		} else if (
+			e instanceof FirebaseError &&
+			errorsToInputs &&
+			errorsToInputs[e.code as FirebaseErrors]
+		) {
+			field = errorsToInputs[e.code as FirebaseErrors] as Path<F>;
+		}
+		if (field)
+			setError(field, { message }, shouldFocus ? { shouldFocus } : undefined);
+		else setGeneralError(message);
+	};
+
+	return { generalError, onError, refreshError };
+};
